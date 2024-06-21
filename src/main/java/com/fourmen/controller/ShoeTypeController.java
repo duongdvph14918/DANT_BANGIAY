89c55fdb13ff64530d38130f90d5279c6d206c36
duongdvph14918 <duongdvph14918@fpt.edu.vn>
2024-06-21 23:30:40 +0700

    duong

--- src/main/java/com/fourmen/controller/ShoeTypeController.java
+++ src/main/java/com/fourmen/controller/ShoeTypeController.java
@@ -14,4 +14,5 @@ public class ShoeTypeController {
 //			return"/user/product/sanPham";
 //		}
 	
+	
 }
--- target/classes/static/assets/js/java.js
+++
@@ -1,4 +0,0 @@
-function search() {
-	var number = document.getElementById("keyword").value;
-	window.location.assign("/4MEN/account/history/search?keyword" + "=" + number);
-}
